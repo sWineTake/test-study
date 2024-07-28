45b89062a0f450621b19d22b17f44c5010eba5e9
twocowsong <twocowsong@naver.com>
2024-07-28 16:19:51 +0900

    first commit

---
+++ part02/src/main/java/com/tester/part02/Part02Application.java
@@ -0,0 +1,13 @@
+package com.tester.part02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part02Application.class, args);
+	}
+
+}
---
+++ part02/src/test/java/com/tester/part02/Part02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tester.part02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
