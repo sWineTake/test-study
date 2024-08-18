b12fb70a5561fcce4ed750f7bc56ce839eed5d7f
twocowsong <twocowsong@naver.com>
2024-08-18 11:06:10 +0900

    테스트 코드 추가
    MockBean을 이용한 PUT, DELETE 테스트

--- part02/src/test/java/com/tester/part02/controller/Ch03Clip02WebMvcTest.java
+++ part02/src/test/java/com/tester/part02/controller/Ch03Clip02WebMvcTest.java
@@ -3,12 +3,13 @@ package com.tester.part02.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tester.part02.adapter.in.api.ArticleController;
 import com.tester.part02.adapter.in.api.dto.ArticleDto;
+import com.tester.part02.adapter.in.api.dto.BoardDto;
 import com.tester.part02.application.port.in.CreateArticleUseCase;
 import com.tester.part02.application.port.in.DeleteArticleUseCase;
 import com.tester.part02.application.port.in.GetArticleUseCase;
 import com.tester.part02.application.port.in.ModifyArticleUseCase;
+import com.tester.part02.common.exception.AccessDeniedException;
 import com.tester.part02.common.exception.ResourceNotFoundException;
-import com.tester.part02.domain.Article;
 import com.tester.part02.domain.factory.ArticleFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -26,8 +27,9 @@ import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -87,7 +89,7 @@ public class Ch03Clip02WebMvcTest {
 
 		mockMvc.perform(get("/articles?boardId={boardId}", 5L))
 			.andDo(print())
-			.andExpect(status().isOk());
+			.andExpect(status().is2xxSuccessful());
 	}
 
 	@Nested
@@ -148,6 +150,74 @@ public class Ch03Clip02WebMvcTest {
 
 	}
 
+	@Nested
+	@DisplayName("PUT /articles")
+	class PutArticle {
+		@Test
+		public void 변경된_ARTICLE의_ARTICLE_ID_반환() throws Exception {
+			// given
+			var modifiedArticle = ArticleFixtures.article();
+			given(modifyArticleUseCase.modifyArticle(any())).willReturn(modifiedArticle);
 
+			// when
+			var body = objectMapper.writeValueAsString(
+				new ArticleDto.UpdateArticleRequest(5L, new BoardDto(1L, "name"), "subject", "content", "username")
+			);
+
+			// then
+			mockMvc.perform(
+					put("/articles")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(body)
+				).andDo(print())
+				.andExpect(status().isOk());
+		}
+
+		@ParameterizedTest(name = "{0}")
+		@CsvSource(
+			value = {
+				"subject is null,,content,user",
+				"content is null,subject,,user",
+			}
+		)
+		public void 파라미터_에러케이스(String desc, String subject, String content, String username) throws Exception{
+			var body = objectMapper.writeValueAsString(
+				new ArticleDto.UpdateArticleRequest(1L, new BoardDto(1L, "name"), subject, content, username)
+			);
+
+			mockMvc.perform(
+				put("/articles")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(body)
+				).andDo(print())
+				.andExpect(status().isBadRequest());
+		}
+
+		@Test
+		public void 권한미달_케이스() throws Exception{
+			given(modifyArticleUseCase.modifyArticle(any()))
+				.willThrow(new AccessDeniedException("다른 작성자는 수정 불가능"));
+
+			var body = objectMapper.writeValueAsString(
+				new ArticleDto.UpdateArticleRequest(5L, new BoardDto(1L, "name"), "subject", "content", "otherUsername")
+			);
+
+			mockMvc.perform(put("/articles").contentType(MediaType.APPLICATION_JSON).content(body))
+				.andDo(print())
+				// AdviceController에서 403에러를 출력하게 해두었기에 테스트코드 추가함
+				.andExpect(status().isForbidden());
+		}
+	}
+
+	@Test
+	public void 삭제테스트() throws Exception {
+		willDoNothing().given(deleteArticleUseCase).deleteArticle(any());
+
+		mockMvc.perform(
+			delete("/articles/{articleId}", 1L)
+		).andDo(print()).andExpect(status().isOk());
+
+		verify(deleteArticleUseCase).deleteArticle(1L);
+	}
 
 }
