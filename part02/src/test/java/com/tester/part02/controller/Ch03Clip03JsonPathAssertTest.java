02842ed2a2f2b7b4f207e8d7554930c22e9d3851
twocowsong <twocowsong@naver.com>
2024-08-21 21:28:02 +0900

    테스트 코드 추가
    JsonPath를 이용한 Post, PUT 작업

--- part02/src/test/java/com/tester/part02/controller/Ch03Clip03JsonPathAssertTest.java
+++ part02/src/test/java/com/tester/part02/controller/Ch03Clip03JsonPathAssertTest.java
@@ -1,12 +1,17 @@
 package com.tester.part02.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tester.part02.adapter.in.api.ArticleController;
+import com.tester.part02.adapter.in.api.dto.ArticleDto;
+import com.tester.part02.adapter.in.api.dto.BoardDto;
 import com.tester.part02.application.port.in.CreateArticleUseCase;
 import com.tester.part02.application.port.in.DeleteArticleUseCase;
 import com.tester.part02.application.port.in.GetArticleUseCase;
 import com.tester.part02.application.port.in.ModifyArticleUseCase;
 import com.tester.part02.common.exception.ResourceNotFoundException;
+import com.tester.part02.domain.Article;
+import com.tester.part02.domain.Board;
 import com.tester.part02.domain.factory.ArticleFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -14,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -21,7 +27,7 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -102,5 +108,97 @@ public class Ch03Clip03JsonPathAssertTest {
 			);
 	}
 
+	@Nested
+	@DisplayName("POST Article")
+	public class PostArticle {
+		@Test
+		@DisplayName("생성된 Article ID 반환")
+		public void returnArticleId() throws Exception {
+			var article = ArticleFixtures.article(1L);
+
+			var body = objectMapper.writeValueAsString(
+				new ArticleDto.CreateArticleRequest(1L, "subject", "content", "username")
+			);
+
+			given(createArticleUseCase.createArticle(any())).willReturn(article);
+
+			mockMvc.perform(
+				post("/articles")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(body)
+				)
+				.andExpectAll(
+					status().isOk(),
+					jsonPath("$.id").value(1L),
+					jsonPath("$.id").exists() // 해당값이 존재한다면 true
+				);
+		}
+
+		@Test
+		@DisplayName("수정 Article ID 반환")
+		public void modifyArticleId() throws Exception {
+			var article = ArticleFixtures.article(1L);
+			var board = new BoardDto(10L, "boardName");
+
+			var body = objectMapper.writeValueAsString(
+				new ArticleDto.UpdateArticleRequest(1L, board, "subject","content", "username")
+			);
+
+			given(modifyArticleUseCase.modifyArticle(any())).willReturn(article);
+
+			mockMvc.perform(
+					put("/articles")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(body)
+				)
+				.andExpectAll(
+					status().isOk(),
+					jsonPath("$.id").value(1L),
+					jsonPath("$.id").exists() // 해당값이 존재한다면 true
+				);
+		}
+	}
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
